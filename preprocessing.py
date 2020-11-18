c0479d5873746fffcd1c60520eda3f7ffa4b2861
Aimee Moses <62718146+armoses@users.noreply.github.com>
2020-11-18 18:47:34 -0500

    Create preprocess function

--- preprocessing.py
+++ preprocessing.py
@@ -11,6 +11,7 @@ import pandas as pd
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 
 #%% Load and explore data
 
@@ -98,110 +99,119 @@ plt.show()
 # and see what gets the best performance...?
 
 
-# Clean text
-def clean_text(str_in):
-    str_out = re.sub("[^A-z0-9-]+", " ", str_in.lower())
-    return str_out
-
-clean_data = raw_data.copy()
-for i in clean_data.columns: # get cols
-    clean_data[i].loc[clean_data[i].isna() == False] = clean_data[i][clean_data[i].isna() == False].apply(clean_text)  
-    clean_data[i].replace(np.nan, '', inplace=True)
-
-# Remove stop words
-from nltk.corpus import stopwords 
-from nltk.tokenize import word_tokenize 
-stop_words = stopwords.words('english')
-
-def remove_stopwords(str_in): # Made this a function because the line got too long with the NaN stuff
-    str_out = word_tokenize(str_in) 
-    str_out = [word for word in str_out if word not in (stop_words)]
-    str_out = ' '.join(str_out)
-    return str_out
-
-no_stopw_data = clean_data.copy()
-for i in no_stopw_data.columns: 
-    no_stopw_data[i] = no_stopw_data[i].apply(remove_stopwords)  
-    # I was getting an error with NaNs
-    # I think the stopword dictionary is lowercase, so our text needed to be too (and without punctuation)
-
-
-# Stem 
-from nltk.stem import PorterStemmer
-ps = PorterStemmer()
-
-stem_data = no_stopw_data.copy()
-for i in stem_data.columns: 
-    stem_data[i] = stem_data[i].apply(lambda x: " ".join([ps.stem(word) for word in word_tokenize(x)]))
-
-
-# Train-test split
-# Split to 8:2 training:test and mimic full distribution of labels in train and test data
-from sklearn.model_selection import train_test_split
-train_data, test_data, train_labels, test_labels = train_test_split(stem_data[['sample_name', 'description', 'transcription', 'keywords']], 
-                                                                    stem_data['medical_specialty'], test_size=0.2, random_state=5, 
-                                                                    stratify=stem_data['medical_specialty'])  
-       
-# Get dataframes with respective column(s) for each of the options above
-dscrp_train_data = train_data["description"].copy()
-trscrp_train_data = train_data["transcription"].copy()
-samp_name_train_data = train_data["sample_name"].copy()
-kwords_train_data = train_data["keywords"].copy()
-all_train_data = dscrp_train_data +' '+ trscrp_train_data +' '+ samp_name_train_data +' '+ kwords_train_data
-
-dscrp_test_data = test_data["description"].copy()
-trscrp_test_data = test_data["transcription"].copy()
-samp_name_test_data = test_data["sample_name"].copy()
-kwords_test_data = test_data["keywords"].copy()
-all_test_data = dscrp_test_data +' '+ trscrp_test_data +' '+ samp_name_test_data +' '+ kwords_test_data
-
-# Count vectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-
-def vectorize(train_series, test_series):
-    vectorizer = CountVectorizer()
-    train_vec = pd.DataFrame(vectorizer.fit_transform(train_series).toarray())
-    test_vec = pd.DataFrame(vectorizer.transform(test_series).toarray())
-    train_vec.columns = vectorizer.get_feature_names()
-    test_vec.columns = vectorizer.get_feature_names()
-    return train_vec, test_vec
-
-def get_df_name(df):
-    name =[x for x in globals() if globals()[x] is df][0]
-    return name
-
-train_sets = [all_train_data, dscrp_train_data, trscrp_train_data, samp_name_train_data, kwords_train_data]
-test_sets = [all_test_data, dscrp_test_data, trscrp_test_data, samp_name_test_data, kwords_test_data]
-vec_dict = {}
-for train_set, test_set in zip(train_sets, test_sets):
-    train_vec, test_vec = vectorize(train_set, test_set)
-    vec_dict[f'{get_df_name(train_set)}_vec'] = train_vec
-    vec_dict[f'{get_df_name(test_set)}_vec'] = test_vec
-
-# Tf-idf
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-def tfidf(train_series, test_series):
-    vectorizer = TfidfVectorizer()
-    train_vec = pd.DataFrame(vectorizer.fit_transform(train_series).toarray())
-    test_vec = pd.DataFrame(vectorizer.transform(test_series).toarray())
-    train_vec.columns = vectorizer.get_feature_names()
-    test_vec.columns = vectorizer.get_feature_names()
-    return train_vec, test_vec
-
-tfidf_dict = {}
-for train_set, test_set in zip(train_sets, test_sets):
-    train_tfidf, test_tfidf = tfidf(train_set, test_set)
-    tfidf_dict[f'{get_df_name(train_set)}_tfidf'] = train_tfidf
-    tfidf_dict[f'{get_df_name(test_set)}_tfidf'] = test_tfidf
+def preprocess(raw_data):
     
-### We now have train and test versions of all columns and each column separately
-## independently fit and transformed by count vectorizer and by tfidf vectorizer.
-## So we have 10 different dataframes to try models on (all the train ones)
+    # Clean text
+    def clean_text(str_in):
+        str_out = re.sub("[^A-z0-9-]+", " ", str_in.lower())
+        return str_out
+    
+    clean_data = raw_data.copy()
+    for i in clean_data.columns: # get cols
+        clean_data[i].loc[clean_data[i].isna() == False] = clean_data[i][clean_data[i].isna() == False].apply(clean_text)  
+        clean_data[i].replace(np.nan, '', inplace=True)
+    
+    
+    # Remove stop words
+    from nltk.corpus import stopwords 
+    from nltk.tokenize import word_tokenize 
+    stop_words = stopwords.words('english')
+    
+    def remove_stopwords(str_in): # Made this a function because the line got too long with the NaN stuff
+        str_out = word_tokenize(str_in) 
+        str_out = [word for word in str_out if word not in (stop_words)]
+        str_out = ' '.join(str_out)
+        return str_out
+    
+    no_stopw_data = clean_data.copy()
+    for i in no_stopw_data.columns: 
+        no_stopw_data[i] = no_stopw_data[i].apply(remove_stopwords)  
+        # I was getting an error with NaNs
+        # I think the stopword dictionary is lowercase, so our text needed to be too (and without punctuation)
+    
+    
+    # Stem 
+    from nltk.stem import PorterStemmer
+    ps = PorterStemmer()
+    
+    stem_data = no_stopw_data.copy()
+    for i in stem_data.columns: 
+        stem_data[i] = stem_data[i].apply(lambda x: " ".join([ps.stem(word) for word in word_tokenize(x)]))
+    
+    
+    # Train-test split
+    # Split to 8:2 training:test and mimic full distribution of labels in train and test data
+    from sklearn.model_selection import train_test_split
+    train_data, test_data, train_labels, test_labels = train_test_split(stem_data[['sample_name', 'description', 'transcription', 'keywords']], 
+                                                                        stem_data['medical_specialty'], test_size=0.2, random_state=5, 
+                                                                        stratify=stem_data['medical_specialty'])  
+           
+    for df in [train_data, test_data, train_labels, test_labels]:
+        df.reset_index(drop=True, inplace=True)
+    # Get dataframes with respective column(s) for each of the options above
+    # and assign names for saving purposes
+    dscrp_train_data = train_data["description"].copy()
+    dscrp_train_data.name = 'dscrp_train_data'
+    trscrp_train_data = train_data["transcription"].copy()
+    trscrp_train_data.name = 'trscrp_train_data'
+    samp_name_train_data = train_data["sample_name"].copy()
+    samp_name_train_data.name = 'samp_name_train_data'
+    kwords_train_data = train_data["keywords"].copy()
+    kwords_train_data.name = 'kwords_train_data'
+    all_train_data = dscrp_train_data +' '+ trscrp_train_data +' '+ samp_name_train_data +' '+ kwords_train_data
+    all_train_data.name = 'all_train_data'
+    
+    dscrp_test_data = test_data["description"].copy()
+    dscrp_test_data.name = 'dscrp_test_data'
+    trscrp_test_data = test_data["transcription"].copy()
+    trscrp_test_data.name = 'trscrp_test_data'
+    samp_name_test_data = test_data["sample_name"].copy()
+    samp_name_test_data.name = 'samp_name_test_data'
+    kwords_test_data = test_data["keywords"].copy()
+    kwords_test_data.name = 'kwords_test_data'
+    all_test_data = dscrp_test_data +' '+ trscrp_test_data +' '+ samp_name_test_data +' '+ kwords_test_data
+    all_test_data.name = 'all_test_data'
+    
+    train_sets = [all_train_data, dscrp_train_data, trscrp_train_data, samp_name_train_data, kwords_train_data]
+    test_sets = [all_test_data, dscrp_test_data, trscrp_test_data, samp_name_test_data, kwords_test_data]
+    
+    
+    # Count vectorizer and tfidf vectorizer
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    
+    def vectorize(train_series, test_series):
+        vectorizer = CountVectorizer()
+        train_vec = pd.DataFrame(vectorizer.fit_transform(train_series).toarray())
+        test_vec = pd.DataFrame(vectorizer.transform(test_series).toarray())
+        train_vec.columns = vectorizer.get_feature_names()
+        test_vec.columns = vectorizer.get_feature_names()
+        return train_vec, test_vec
+    
+    def tfidf(train_series, test_series):
+        vectorizer = TfidfVectorizer()
+        train_vec = pd.DataFrame(vectorizer.fit_transform(train_series).toarray())
+        test_vec = pd.DataFrame(vectorizer.transform(test_series).toarray())
+        train_vec.columns = vectorizer.get_feature_names()
+        test_vec.columns = vectorizer.get_feature_names()
+        return train_vec, test_vec
+    
+    vec_dict = {}
+    tfidf_dict = {}
+    for train_set, test_set in zip(train_sets, test_sets):
+        train_vec, test_vec = vectorize(train_set, test_set)
+        vec_dict[f'{train_set.name}_vec'] = train_vec
+        vec_dict[f'{test_set.name}_vec'] = test_vec
+        
+        train_tfidf, test_tfidf = tfidf(train_set, test_set)
+        tfidf_dict[f'{train_set.name}_tfidf'] = train_tfidf
+        tfidf_dict[f'{test_set.name}_tfidf'] = test_tfidf
+    
+    ### We now have train and test versions of all columns and each column separately
+    ## independently fit and transformed by count vectorizer and by tfidf vectorizer.
+    ## So we have 10 different dataframes to try models on (all the train ones)    
 
-
-
-
+    return train_data, test_data, train_labels, test_labels, vec_dict, tfidf_dict
 
 
 
