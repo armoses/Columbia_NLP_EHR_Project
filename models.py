ae3a066dbc5aa59456b1fb1b3e669ef8382f17ab
Aimee Moses <62718146+armoses@users.noreply.github.com>
2020-11-18 21:41:17 -0500

    PCA and multi-step model

--- models.py
+++ models.py
@@ -8,10 +8,8 @@ Created on Wed Nov 18 14:43:43 2020
 #%% Imports
 
 import pandas as pd
-import re
 import matplotlib.pyplot as plt
 import numpy as np
-import pickle
 from preprocessing import preprocess
 
 #%% Load and preprocess data
@@ -19,13 +17,27 @@ from preprocessing import preprocess
 raw_data = pd.read_csv('mtsamples.csv', index_col=0) 
 train_data, test_data, train_labels, test_labels, vec_dict, tfidf_dict = preprocess(raw_data)
 
+
+#%% Dimensionality Reduction (PCA)
+from sklearn.decomposition import PCA
+pca = PCA(n_components=.95)
+trscrp_vec_train = pd.DataFrame(pca.fit_transform(vec_dict['trscrp_train_data_vec']))
+trscrp_vec_test = pd.DataFrame(pca.transform(vec_dict['trscrp_test_data_vec']))
+
+
 #%% Classic supervised models
+
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(random_state=1).fit(trscrp_vec_train, 
+                                                 train_labels)
+rfc_pred = rfc.predict(trscrp_vec_test)
+class_rep = classification_report(rfc_pred, test_labels, zero_division=0)
 
 
 
 #%% Experimental stuff
 
-# LDA
+##### LDA
 from sklearn.decomposition import LatentDirichletAllocation
 n_labels = len(raw_data.medical_specialty.unique())
 
@@ -73,7 +85,65 @@ from sklearn.metrics import classification_report
 class_rep = classification_report(lda_trscrp_vec_test, test_labels, zero_division=0)
 
 
+#### Binary then multi-class models
+def multi_step(train_df, test_df, train_labels, binary_model, multi_model):
+    train_binary_labels = train_labels.copy()
+    for i in range(len(train_binary_labels)):
+        if train_binary_labels[i] == 'surgeri':
+            train_binary_labels[i] = 1
+        else:
+            train_binary_labels[i] = 0
+    train_binary_labels = train_binary_labels.astype(int)
+            
+    bin_mod = binary_model.fit(train_df, train_binary_labels)
+    binary_pred = bin_mod.predict(test_df)
+    
+    # Get non-surgery and run through next model
+    non_surg_train = train_df[train_labels != 'surgeri']
+    non_surg_train_labels = train_labels[train_labels != 'surgeri']
+    non_surg_test = test_df[binary_pred == 0]
+    
+    mult_mod = multi_model.fit(non_surg_train, non_surg_train_labels)
+    non_surg_predict = mult_mod.predict(non_surg_test)
+    non_surg_predict = pd.Series(non_surg_predict)
+    non_surg_predict.index = non_surg_test.index
+    
+    all_predictions = train_labels[:len(binary_pred)].copy()
+    for i in range(len(binary_pred)):
+        if i in non_surg_predict.index:
+            all_predictions[i] = non_surg_predict[i]
+        else:
+            all_predictions[i] = 'surgeri'
+    
+    return binary_pred, all_predictions
+    
 
-# Binary then multi-class models
+
+test_binary_labels = test_labels.copy()
+for i in range(len(test_binary_labels)):
+    if test_binary_labels[i] == 'surgeri':
+        test_binary_labels[i] = 1
+    else:
+        test_binary_labels[i] = 0
+test_binary_labels = test_binary_labels.astype(int)
+
+# Logistic Regression for binary
+from sklearn.linear_model import LogisticRegression
+log_reg = LogisticRegression()
+
+# Random Forest for non-surgery
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(random_state=1)
+
+binary_pred, all_predictions = multi_step(vec_dict['trscrp_train_data_vec'], 
+                                          vec_dict['trscrp_test_data_vec'],
+                                          train_labels,log_reg, rfc)
+# binary_pred, all_predictions = multi_step(trscrp_vec_train, 
+#                                           trscrp_vec_test,
+#                                           train_labels,log_reg, rfc)
+
+# Evaluate binary and full
+class_rep_bin = classification_report(binary_pred, test_binary_labels, zero_division=0)
+class_rep_full = classification_report(all_predictions, test_labels, zero_division=0)
 
 
